94c34d717093541fe8c4dea6cb2fe732652b507c
Thomas Warren <thomas.warren@suresite.co.uk>
2020-11-25 18:31:44 +0000

    Checked in intial project set up with an ASP.Net Core project for a web based application and a JUnit Project for testing ready to go

---
+++ TechincalTestUnitTests/ProductsTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TechincalTestUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ProductsTest()
+        {
+        }
+    }
+}
---
+++ TechnicalTestTDD/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TechnicalTestTDD
+{
+    public class Class1
+    {
+    }
+}
